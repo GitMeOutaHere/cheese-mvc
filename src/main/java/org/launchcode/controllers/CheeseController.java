d99d6c5dae8de49c6720e76c13784d43b7e261b5
Tom <trshuman.financial@gmail.com>
2017-12-05 08:27:09 -0600

    working through 3.8 studio

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -6,10 +6,7 @@ import org.launchcode.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -45,6 +42,7 @@ public class CheeseController {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Cheese");
+            model.addAttribute("cheeseTypes", CheeseType.values());
             return "cheese/add";
         }
 
@@ -67,6 +65,28 @@ public class CheeseController {
         }
 
         return "redirect:";
+    }
+
+
+
+    @RequestMapping(value = "edit/{cheeseId}", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @PathVariable int cheeseId){
+        CheeseData.getById(cheeseId);
+        model.addAttribute("cheese",CheeseData.getById(cheeseId));
+        model.addAttribute("cheeseTypes", CheeseType.values());
+
+        return "cheese/edit";
+    }
+
+    @RequestMapping(value = "edit/{cheeseId}", method = RequestMethod.POST)
+    public String processEditForm(int cheeseId, String name, String description) {
+
+        Cheese newCheese = CheeseData.getById(cheeseId);
+        newCheese.setName(name);
+        newCheese.setDescription(description);
+
+
+        return "redirect:/cheese";
     }
 
 }
